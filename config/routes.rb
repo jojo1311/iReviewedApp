9a7b8873be9bf736a15766d3ac547eca25cb463a
jojo1311 <josette@cocuyostudio.com>
2015-11-19 09:23:37 -0500

    added session controller and custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  root to: "books#index"
+
   resources :books do
     resources :notes, only: [:create, :destroy]
   end
 
-  root to: "books#index"
+  resources :sessions, only: [:new, :create, :destroy]
+
+#Custom routing 
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout" 
 end
